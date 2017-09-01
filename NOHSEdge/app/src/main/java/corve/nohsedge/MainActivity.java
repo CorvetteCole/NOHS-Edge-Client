d2c6a665b0dd586b1cc5a55a08fbcfe36f12efd7
Cole Gerdemann <corvettecole@gmail.com>
2017-09-01 14:47:09 -0400

    initial commit

---
+++ NOHSEdge/app/src/main/java/corve/nohsedge/MainActivity.java
@@ -0,0 +1,15 @@
+package corve.nohsedge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+         
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
