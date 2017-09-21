a3242aad8d9761efe957d4a781013ad7465f53d7
Cole Gerdemann <corvettecole@gmail.com>
2017-09-21 13:11:38 -0400

    prepping for listing edge class in static shortcut

---
+++ NOHSEdge/app/src/main/java/corve/nohsedge/EdgeViewActivity.java
@@ -0,0 +1,13 @@
+package corve.nohsedge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdgeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edge_view);
+    }
+}
