88935824d268840f824075c6676498ccf8786559
Cole Gerdemann <corvettecole@gmail.com>
2017-09-21 15:39:35 -0400

    Preliminary working list...

--- NOHSEdge/app/src/main/java/corve/nohsedge/EdgeViewActivity.java
+++ NOHSEdge/app/src/main/java/corve/nohsedge/EdgeViewActivity.java
@@ -12,8 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import static corve.nohsedge.MainActivity.DefaultEdgeDay1Value;
 import static corve.nohsedge.MainActivity.DefaultEdgeDay2Value;
@@ -29,82 +33,108 @@ import static corve.nohsedge.MainActivity.PREF_EDGE5;
 import static corve.nohsedge.MainActivity.PREF_MIN;
 
 public class EdgeViewActivity extends AppCompatActivity {
-private int notifyMinutes;
-    private String EdgeDay1;
-    private String EdgeDay2;
-    private String EdgeDay3;
-    private String EdgeDay4;
-    private String EdgeDay5;
+    private String[] EdgeDay = new String[5];
+    private ListView mList;
+    private String[] EdgeTitle = new String[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edge_view);
+        mList = (ListView) findViewById(R.id.listview);
+        loadPreferences();
+
     }
+
     public void loadPreferences() {
         SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME,
                 MainActivity.MODE_PRIVATE);
 
         // Get value
-        String EdgeDay1Value = settings.getString(PREF_EDGE1, DefaultEdgeDay1Value);
-        String EdgeDay2Value = settings.getString(PREF_EDGE2, DefaultEdgeDay2Value);
-        String EdgeDay3Value = settings.getString(PREF_EDGE3, DefaultEdgeDay3Value);
-        String EdgeDay4Value = settings.getString(PREF_EDGE4, DefaultEdgeDay4Value);
-        String EdgeDay5Value = settings.getString(PREF_EDGE5, DefaultEdgeDay5Value);
-        notifyMinutes = settings.getInt(PREF_MIN, DefaultMinValue);
-
-        InterpretEdgeData(EdgeDay1Value);
-        InterpretEdgeData(EdgeDay2Value);
-        InterpretEdgeData(EdgeDay3Value);
-        InterpretEdgeData(EdgeDay4Value);
-        InterpretEdgeData(EdgeDay5Value);
-    }
-
-
-    public void InterpretEdgeData(String consoleMessage){
-        if (consoleMessage.toLowerCase().contains("Mon".toLowerCase())){
-            EdgeDay1 = consoleMessage;
-            Log.d("Monday Edge Class", EdgeDay1);
-        }
-        if (consoleMessage.toLowerCase().contains("Tue".toLowerCase())){
-            EdgeDay2 = consoleMessage;
-            Log.d("Tuesday Edge Class", EdgeDay2);
-        }
-        if (consoleMessage.toLowerCase().contains("Wed".toLowerCase())){
-            EdgeDay3 = consoleMessage;
-            Log.d("Wednesday Edge Class", EdgeDay3);
-        }
-        if (consoleMessage.toLowerCase().contains("Thu".toLowerCase())){
-            EdgeDay4 = consoleMessage;   //Thursday
-            Log.d("Thursday Edge Class", EdgeDay4);
-        }
-        if (consoleMessage.toLowerCase().contains("Fri".toLowerCase())){
-            EdgeDay5 = consoleMessage;
-            Log.d("Friday Edge Class", EdgeDay5);
+        EdgeDay[0] = settings.getString(PREF_EDGE1, DefaultEdgeDay1Value);
+        EdgeDay[1] = settings.getString(PREF_EDGE2, DefaultEdgeDay2Value);
+        EdgeDay[2] = settings.getString(PREF_EDGE3, DefaultEdgeDay3Value);
+        EdgeDay[3] = settings.getString(PREF_EDGE4, DefaultEdgeDay4Value);
+        EdgeDay[4] = settings.getString(PREF_EDGE5, DefaultEdgeDay5Value);
+        int i = 0;
+        while (i <= 4) {
+            InterpretEdgeData(EdgeDay[i]);
+            i++;
         }
     }
 
 
-    public String parseEdgeTitle(String EdgeString){
-        EdgeString = EdgeString.substring(EdgeString.indexOf(">") + 1);
-        EdgeString = EdgeString.substring(0, EdgeString.indexOf("</h3>"));
+    public void InterpretEdgeData(String consoleMessage) {
+        if (consoleMessage.toLowerCase().contains("Mon".toLowerCase())) {
+            EdgeDay[0] = consoleMessage;
+            Log.d("Monday Edge Class", EdgeDay[0]);
+        }
+        if (consoleMessage.toLowerCase().contains("Tue".toLowerCase())) {
+            EdgeDay[1] = consoleMessage;
+            Log.d("Tuesday Edge Class", EdgeDay[1]);
+        }
+        if (consoleMessage.toLowerCase().contains("Wed".toLowerCase())) {
+            EdgeDay[2] = consoleMessage;
+            Log.d("Wednesday Edge Class", EdgeDay[2]);
+        }
+        if (consoleMessage.toLowerCase().contains("Thu".toLowerCase())) {
+            EdgeDay[3] = consoleMessage;   //Thursday
+            Log.d("Thursday Edge Class", EdgeDay[3]);
+        }
+        if (consoleMessage.toLowerCase().contains("Fri".toLowerCase())) {
+            EdgeDay[4] = consoleMessage;
+            Log.d("Friday Edge Class", EdgeDay[4]);
+        }
+        int i = 0;
+        while (i != 5) {
+            if (EdgeDay[i] != null) {
+                EdgeTitle[i] = parseEdgeTitle(EdgeDay[i]);
+            }
+            else {
+                EdgeTitle[i] = "does not exist!";
+            }
+            i++;
+        }
+        setList();
+    }
+
+    public String parseEdgeTitle(String EdgeString) {
+        if (EdgeString.toLowerCase().contains("h3")) {
+            EdgeString = EdgeString.substring(EdgeString.indexOf(">") + 1);
+            EdgeString = EdgeString.substring(0, EdgeString.indexOf("</h3>"));
+        }
+        else {
+            EdgeString = "does not exist!";
+        }
         return EdgeString;
     }
 
-    public String parseEdgeTime(String EdgeString){
+    public String parseEdgeTime(String EdgeString) {
         String session = "";
-        if (EdgeString.toLowerCase().contains("12:43")){
+        if (EdgeString.toLowerCase().contains("12:43")) {
             session = "12:43";
         }
-        if (EdgeString.toLowerCase().contains("1:09")){
+        if (EdgeString.toLowerCase().contains("1:09")) {
             session = "1:09";
         }
         return session;
     }
-    public String parseEdgeText(String EdgeString){
+
+    public String parseEdgeText(String EdgeString) {
         EdgeString = EdgeString.substring(EdgeString.indexOf("g>") + 2);
         EdgeString = EdgeString.substring(0, EdgeString.indexOf("</"));
         return EdgeString;
     }
 
-}
+    public void setList() {
+        int i = 0;
+        while (i <= 4) {
+            Log.d("EdgeTitle" + i, EdgeTitle[i]);
+            i++;
+        }
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, EdgeTitle);
+            mList.setAdapter(adapter);
+        }
+
+    }
+
