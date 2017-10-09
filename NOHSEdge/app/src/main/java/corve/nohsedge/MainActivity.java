86ac637135056059f5f9a3a3af47765e29bb65f3
CorvetteCole <corvettecole@gmail.com>
2017-10-09 17:31:55 -0400

    home is auto selected after login now

--- NOHSEdge/app/src/main/java/corve/nohsedge/MainActivity.java
+++ NOHSEdge/app/src/main/java/corve/nohsedge/MainActivity.java
@@ -226,6 +226,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_homescreen);
         getSupportActionBar().setTitle("Home");
     }
 
